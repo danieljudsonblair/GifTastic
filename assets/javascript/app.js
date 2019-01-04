b1a8011203f96659449da2835841bb5d9d1e5e93
s1191 <danieljudsonblair@gmail.com>
2019-01-04 00:09:32 -0500

    All but animation stop/start and some formatting(?) done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,37 +24,37 @@ function createButtons() {
         $("#buttons-view").append(a);
     }
 
-$("button").on("click", function() {
-    var rand = Math.floor(Math.random() * 100) + 1;
-    $(".gif-tainer").empty();
-    var celeb = $(this).attr("data-name");
-    console.log(celeb);
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    celeb + "&offset=" + rand + "&limit=10&api_key=p1XhbNzWjNxNR2SeaRfB9v3Ri8evAWdv";
+    $("button").on("click", function () {
+        var rand = Math.floor(Math.random() * 100) + 1;
+        $(".gif-tainer").empty();
+        var celeb = $(this).attr("data-name");
+        console.log(celeb);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            celeb + "&offset=" + rand + "&limit=10&api_key=p1XhbNzWjNxNR2SeaRfB9v3Ri8evAWdv";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    .then(function(response) {
-        var results = response.data;
-        for (let j = 0; j < results.length; j++) {
-            var celebDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[j].rating);
-            var celebImage = $("<img>");
-            celebImage.attr("src", results[j].images.fixed_height_still.url);
-            celebDiv.append(celebImage);
-            celebDiv.append(p);
-            celebDiv.attr("class", "c" + [j]);
-            celebDiv.attr("id", "gifs");
-            $(".gif-tainer").prepend(celebDiv);
-        }
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                var results = response.data;
+                for (let j = 0; j < results.length; j++) {
+                    var celebDiv = $("<div>");
+                    var p = $("<p>").text("Rating: " + results[j].rating);
+                    celebImage = $("<img>");
+                    celebImageS.attr("src", results[j].images.fixed_height_still.url);
+                    celebDiv.append(celebImage);
+                    celebDiv.append(p);
+                    celebDiv.attr("class", "c" + [j]);
+                    celebDiv.attr("id", "gifs");
+                    $(".gif-tainer").prepend(celebDiv);
+                }
+            });
     });
-});
 }
 createButtons();
 
-$("#add-celeb").on("click", function() {
+$("#add-celeb").on("click", function () {
     event.preventDefault();
     var newCeleb = $("#celeb-input").val().trim();
     topics.push(newCeleb);
